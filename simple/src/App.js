ecf550f8ae0f0d87696c7447c020027ad0468948
Jhon Rocha <jhmrocha@gmail.com>
2023-09-27 21:53:25 -0300

    Simple components

---
+++ simple/src/App.js
@@ -0,0 +1,25 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ])
+}
+
+
+const App = () => {
+  return new React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, { animal: 'Dog', name: 'Dunkan', breed: 'Husky'}),
+      React.createElement(Pet, { animal: 'Bird', name: 'Pepper', breed: 'Cockatiel'}),
+      React.createElement(Pet, { animal: 'Cat', name: 'Doink', breed: 'Mixed'}),
+    ]
+  )
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
