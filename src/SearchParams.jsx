7668eacaba9444e7e5d610003e6af1995dde701d
Jhon Rocha <jhmrocha@gmail.com>
2023-09-28 09:58:14 -0300

    multiple hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+const ANIMALS = ["bird", "dog", "cat", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const BREEDS = [];
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,36 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={BREEDS.length === 0}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
