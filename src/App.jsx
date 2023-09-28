93256a8a4bafb43e1471ab17e9ab06d47b89ec79
Jhon Rocha <jhmrocha@gmail.com>
2023-09-28 08:48:21 -0300

    useState hook + eslint for hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Dunkan" animal="Dog" breed="Husky"/>
+      <SearchParams/>
     </div>
   );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
---
+++ src/SearchParams.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
