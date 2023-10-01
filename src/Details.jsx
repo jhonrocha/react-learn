058fd5a0363586af15974a4ce6207333f13c24fd
Jhon Rocha <jhmrocha@gmail.com>
2023-10-01 19:24:43 -0300

    react-query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
 
 export default function Details() {
   const { id } = useParams();
-  return <h1>{id}</h1>;
+  const result = useQuery(["pet_details", id], fetchPet);
+  if (result.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+  const pet = result.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city} - ${pet.state}`}</h2>
+        <button>Adopt me</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 }
---
+++ src/fetchPet.js
@@ -0,0 +1,8 @@
+export default async function fetchPet({ queryKey }) {
+  const id = queryKey[1];
+  const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  if (!apiRes.ok) {
+    throw new Error(`details/${id} fetch not ok`);
+  }
+  return apiRes.json();
+}
